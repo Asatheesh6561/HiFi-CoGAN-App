48b178f10d68e201e6ca09f347e74b7dbac10314
Asatheesh6561 <86309616+Asatheesh6561@users.noreply.github.com>
2023-08-02 21:35:18 -0400

    add FastAPI to flask backend

--- director/send.py
+++ director/send.py
@@ -1,22 +1,29 @@
-from flask import Flask, request, send_file
+from fastapi import FastAPI, HTTPException, UploadFile, File, Response
 import os
 import subprocess
-app = Flask(__name__)
-@app.route('/')
-def homePage():
-    return "home page"
 
-@app.route('/fileUpload', methods=['POST'])
-def fileUpload():
-    file = request.form(['sample'])
-    file.save(os.path.join('input_waves', file.filename))
-    f = open('filenames.txt', 'w')
-    f.write(f'{file.filename}\n')
-    subprocess.run(['sh', './send.sh', file.filename])
-    return "File uploaded successfully"
+app = FastAPI()
 
-@app.route('/download')
-def download_file():
-    with open('filenames.txt') as f:
-        new_filename = os.path.join('output_waves', f.strip())
-        return send_file(new_filename, as_attachment=True)
+@app.get('/')
+def home_page():
+    return "Home page"
+
+@app.post('/fileUpload/')
+async def file_upload(sample: UploadFile = File(...)):
+    filename = os.path.join('input_waves', sample.filename)
+    with open(filename, 'wb') as f:
+        content = await sample.read()
+        f.write(content)
+    with open('filenames.txt', 'w') as f:
+        f.write(f'{sample.filename}\n')
+    subprocess.run(['sh', './send.sh', sample.filename])
+    return {"message": "File uploaded successfully"}
+
+@app.get('/download/')
+async def download_file():
+    try:
+        with open('filenames.txt') as f:
+            new_filename = os.path.join('output_waves', f.readline().strip())
+            return Response(new_filename, media_type='application/octet-stream', headers={'Content-Disposition': f'attachment; filename={os.path.basename(new_filename)}'})
+    except FileNotFoundError:
+        raise HTTPException(status_code=404, detail="File not found")
